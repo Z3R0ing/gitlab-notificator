cf37c133ce85c4ad065270910cf92817d1862aa4
Daniil Lytin <lytin.daniil@gmail.com>
2024-09-06 11:39:39 +0300

    init commit

---
+++ src/main/java/ru/z3r0ing/gitlabnotificator/GitlabNotificatorApplication.java
@@ -0,0 +1,13 @@
+package ru.z3r0ing.gitlabnotificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabNotificatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabNotificatorApplication.class, args);
+	}
+
+}
