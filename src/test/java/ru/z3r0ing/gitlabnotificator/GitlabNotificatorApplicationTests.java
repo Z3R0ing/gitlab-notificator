0100586822835e768e277ec29c84423591538f6d
Z3R0ing <lytin.daniil@gmail.com>
2025-08-23 12:34:33 +0800

    init commit

---
+++ src/test/java/ru/z3r0ing/gitlabnotificator/GitlabNotificatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.z3r0ing.gitlabnotificator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabNotificatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
