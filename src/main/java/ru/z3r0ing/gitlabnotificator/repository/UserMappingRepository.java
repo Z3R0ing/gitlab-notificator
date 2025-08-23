1d5067b619e31af76efa65850076b1336fef3c1f
Z3R0ing <lytin.daniil@gmail.com>
2025-08-24 00:20:39 +0800

    added finding all users with specific role

--- src/main/java/ru/z3r0ing/gitlabnotificator/repository/UserMappingRepository.java
+++ src/main/java/ru/z3r0ing/gitlabnotificator/repository/UserMappingRepository.java
@@ -3,11 +3,14 @@ package ru.z3r0ing.gitlabnotificator.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.z3r0ing.gitlabnotificator.model.UserMapping;
+import ru.z3r0ing.gitlabnotificator.model.UserRole;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserMappingRepository extends JpaRepository<UserMapping, Long> {
     Optional<UserMapping> findByTelegramId(Long telegramId);
     Optional<UserMapping> findByGitlabUserId(Long gitlabUserId);
+    List<UserMapping> findAllByRole(UserRole role);
 }
