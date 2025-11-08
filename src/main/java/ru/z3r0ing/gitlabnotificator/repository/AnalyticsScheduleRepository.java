c4c56ba1da492088813063ea985f07e31e49ebb6
Z3R0ing <lytin.daniil@gmail.com>
2025-11-08 14:42:06 +0800

    remove unused entity

--- src/main/java/ru/z3r0ing/gitlabnotificator/repository/AnalyticsScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.z3r0ing.gitlabnotificator.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.z3r0ing.gitlabnotificator.model.entity.AnalyticsSchedule;
-
-@Repository
-public interface AnalyticsScheduleRepository extends JpaRepository<AnalyticsSchedule, Long> {
-}
